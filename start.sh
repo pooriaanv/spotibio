95d2e787b9bebf782a1b3831ce04137b682fe09f
Pooria Anvari <masonalex540@gmail.com>
2023-10-19 00:36:49 +0330

    Fix compatibility for start.sh with 'docker-compose' and 'docker compose' command

--- start.sh
+++ start.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-docker_compose="docker compose"
-if ! [ $(command -v "$docker_compose") ]; then
-    docker_compose="docker-compose"
-fi
-
 if [ "$1" == "rebuild" ]; then
-  "$docker_compose" down -v && "$docker_compose" compose up -d --build --force-recreate
+  if type docker-compose > /dev/null; then
+    docker-compose down -v && docker-compose up -d --build --force-recreate
+  else
+    docker compose down -v && docker compose up -d --build --force-recreate
+  fi
 else
- "$docker_compose" up -d
+  if type docker-compose > /dev/null; then
+    docker-compose up -d
+  else
+    docker compose up -d
+  fi
 fi
